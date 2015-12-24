7915c95bbec6d7419ebe461baac35c38f6424a2a
Nick Artman <nick@addosolutions.com>
2015-12-24 14:42:39 -0500

    Adding some basic types

--- drivers/ip/some-network-device.js
+++ drivers/ip/some-network-device.js
@@ -1,5 +1,13 @@
 import BasicNetworkDriver from '/lib/BasicNetworkDriver';
+import Types from '/lib/Types';
 
 export default class SomeNetworkDevice extends BasicNetworkDriver{
+
+	/**
+	 * Registers what this particular device is capable of
+	 */
+	registerAbilities(){
+
+	}
 
 }
--- drivers/some-serial-device.js
+++ drivers/some-serial-device.js
@@ -1,5 +1,32 @@
 import BasicDriver from '/lib/BasicDriver';
+import Types from '/lib/Types';
 
 export default class SomeSerialDevice extends BasicDriver{
+
+	/**
+	 * Registers what this particular device is capable of
+	 */
+	registerAbilities(){
+
+		// Power Button
+		this.registerAbility(Types.Button, {
+			name:"Power",
+			press: function(){
+				// Send Serial Command Here
+			}
+		})
+
+
+		// Brightness
+		this.registerAbility(Types.Button, {
+			name:"Brightness",
+			min: 1,
+			max: 100,
+			change: function(newValue){
+				// Send Serial Command Here
+			}
+		})
+
+	}
 
 }
